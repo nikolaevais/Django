da035b45f2aff754196cc2d89b858cb167193823
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-09-04 00:25:22 +0300

    created a blog application implemented CRUD based on CBV for the blog

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from users.models import NULLABLE
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Содержимое статьи', **NULLABLE)
+    image = models.ImageField(upload_to='blog/photo', verbose_name='Изображение', **NULLABLE)
+    views_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
+        ordering = ['title', 'description']
--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('users/', include('users.urls', namespace='users')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
